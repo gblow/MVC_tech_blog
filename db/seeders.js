a687cc4faeb7615b8daf907b000146d53b50a468
gblow <144559335+gblow@users.noreply.github.com>
2024-04-12 08:23:00 -0700

    Update seeders.js

--- db/seeders.js
+++ db/seeders.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 require('dotenv').config();
 
 async function seedDatabase() {
